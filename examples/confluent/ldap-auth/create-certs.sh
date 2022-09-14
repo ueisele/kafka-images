45be39e5e829e281ba8430717f16b6cd600e1310
Uwe Eisele <code@uweeisele.eu>
2022-09-14 12:21:56 +0200

    examples: quotas example for confluent ldap

--- examples/confluent/ldap-auth/create-certs.sh
+++ examples/confluent/ldap-auth/create-certs.sh
@@ -9,11 +9,15 @@ create_certificates()
 
   # Generating public and private keys for token signing
   echo "Generating public and private keys for token signing"
-  docker run -v ${DIR}/security/:/etc/kafka/secrets/ -u0 confluentinc/cp-server:${CONFLUENT_VERSION} bash -c "mkdir -p /etc/kafka/secrets/keypair; openssl genrsa -out /etc/kafka/secrets/keypair/keypair.pem 2048; openssl rsa -in /etc/kafka/secrets/keypair/keypair.pem -outform PEM -pubout -out /etc/kafka/secrets/keypair/public.pem && chown -R $(id -u $USER):$(id -g $USER) /etc/kafka/secrets/keypair"
+  if command -v podman &> /dev/null; then
+    podman run -v ${DIR}/security/:/etc/kafka/secrets/:z -u0 docker.io/confluentinc/cp-server:${CONFLUENT_VERSION} bash -c "mkdir -p /etc/kafka/secrets/keypair; openssl genrsa -out /etc/kafka/secrets/keypair/keypair.pem 2048; openssl rsa -in /etc/kafka/secrets/keypair/keypair.pem -outform PEM -pubout -out /etc/kafka/secrets/keypair/public.pem"
+  else
+    docker run -v ${DIR}/security/:/etc/kafka/secrets/:z -u0 docker.io/confluentinc/cp-server:${CONFLUENT_VERSION} bash -c "mkdir -p /etc/kafka/secrets/keypair; openssl genrsa -out /etc/kafka/secrets/keypair/keypair.pem 2048; openssl rsa -in /etc/kafka/secrets/keypair/keypair.pem -outform PEM -pubout -out /etc/kafka/secrets/keypair/public.pem && chown -R $(id -u $USER):$(id -g $USER) /etc/kafka/secrets/keypair"
+  fi
 
   # Enable Docker appuser to read files when created by a different UID
   echo -e "Setting insecure permissions on some files in ${DIR}/../security for demo purposes\n"
-  chmod 644 ${DIR}/security/keypair/keypair.pem
+  chmod -R ug=rwX,o=rX ${DIR}/security/keypair/
 }
 
 create_certificates
